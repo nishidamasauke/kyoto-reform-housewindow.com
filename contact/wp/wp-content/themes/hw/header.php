4ac3753aefb79c3753bc57c5129410cb4b703308
nishidamasauke <46141334+nishidamasauke@users.noreply.github.com>
2019-01-24 16:40:00 +0900

    修正対応

--- contact/wp/wp-content/themes/hw/header.php
+++ contact/wp/wp-content/themes/hw/header.php
@@ -16,6 +16,64 @@
   <link rel="stylesheet" href="/common/css/common.css">
   <link rel="stylesheet" href="/common/css/animate.css">
   <link rel="stylesheet" href="/contact/css/style.css">
+<script type="application/ld+json">
+[
+{
+"@context": "http://schema.org",
+"@type": "BreadcrumbList",
+"itemListElement": [
+{
+"@type": "ListItem",
+"position": 1,
+"item": {
+"@id": "https://kyoto-reform-housewindow.com/",
+"name": "HOME"
+}
+}, 
+{
+"@type": "ListItem",
+"position": 2,
+"item": {
+"@id": "https://kyoto-reform-housewindow.com/contact/",
+"name": "お問い合わせ"
+}
+}
+]
+},
+{
+"@context": "https://schema.org",
+"@type": "SiteNavigationElement",
+"name": [
+"ホーム",
+"リフォームサービス紹介",
+"リフォーム施工事例",
+"会社案内",
+"プライバシーポリシー",
+"サイトマップ",
+"お問い合わせ"
+],
+"url": [
+"https://kyoto-reform-housewindow.com/",
+"https://kyoto-reform-housewindow.com/service/",
+"https://kyoto-reform-housewindow.com/work/",
+"https://kyoto-reform-housewindow.com/company/",
+"https://kyoto-reform-housewindow.com/privacy/",
+"https://kyoto-reform-housewindow.com/sitemap/",
+"https://kyoto-reform-housewindow.com/contact/"
+]
+},
+{
+"@context": "http://schema.org",
+"@type": "WebSite",
+"mainEntityOfPage": {
+"@type": "WebPage",
+"@id": "https://kyoto-reform-housewindow.com/contact/"
+},
+"headline": "お問い合わせ｜株式会社ハウスウィンドウ：京都",
+"description": "リフォームに関するあらゆるお困り事、ご相談、お気軽にお問い合わせください。現地調査、お見積り無料、リフォームローンや補助金の事もご対応可能です。"
+}
+]
+</script>
 </head>
 <body>
 <div id="container" class="container">
@@ -26,15 +84,15 @@
   </div>
 </div>
 <div class="header_mid row">
-  <h1 class="header_mid_logo">
+  <p class="header_mid_logo">
     <a href="/">
       <picture>
         <source media="(max-width: 768px)" srcset="../common/images/logo_sp.png" />
         <img src="/common/images/logo_pc.png?v=9a9881d452a1e3fc573f4e7e4062e68c" alt="リフォーム専門店株式会社ハウスウィンドウ" />
       </picture>
     </a>
-  </h1>
-  <p>京都のリフォーム専門店ハウスウィンドウ</p>
+  </p>
+  <h1>京都のリフォーム専門店ハウスウィンドウ</h1>
   <div class="header_mid_listlink show_pc" id="header_mid_listlink">
     <ul>
       <li>
@@ -75,8 +133,8 @@
       <li><a href="/work">リフォーム施工事例</a></li>
       <li><a href="/company">会社案内</a></li>
       <li class="show_pc"><a href="/contact" class="btn_top"><span>お問い合わせ</span></a></li>
-      <li class="show_sp"><a href="">プライバシーポリシー</a></li>
-      <li class="show_sp"><a href="">サイトマップ</a></li>
+      <li class="show_sp"><a href="/privacy/">プライバシーポリシー</a></li>
+      <li class="show_sp"><a href="/sitemap/">サイトマップ</a></li>
     </ul>　　　 　　　 　　　
   </nav>
 </div>
